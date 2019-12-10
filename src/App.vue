1428093fb5574873af09f57053d42b8820c99987
Chris Day <montymontgomeri@gmail.com>
2019-12-10 10:41:23 -0500

    Created components folder, header & footer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <template>
   <div id="app">
+    <header></header>
     <div id="nav">
       <router-link to="/">Home</router-link>
     </div>
     <router-view/>
+    <footer></footer>
   </div>
 </template>
+
+
+<script>
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
