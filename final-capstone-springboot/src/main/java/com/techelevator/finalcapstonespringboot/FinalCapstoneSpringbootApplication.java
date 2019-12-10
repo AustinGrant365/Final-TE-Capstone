db3dc59ff0dc71b0aa48d6d27e22dd03df09ea14
nathantyler <56833450+nathantyler@users.noreply.github.com>
2019-12-10 13:01:51 -0500

    Testing spring boot. Initialized project with start.spring.io

---
+++ final-capstone-springboot/src/main/java/com/techelevator/finalcapstonespringboot/FinalCapstoneSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.techelevator.finalcapstonespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalCapstoneSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalCapstoneSpringbootApplication.class, args);
+	}
+
+}
