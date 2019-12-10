b74008f3238618dcb2408f2c04abf732b6c6ecad
Chris Day <montymontgomeri@gmail.com>
2019-12-10 12:00:37 -0500

    Got the header to work!

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="register" class="text-center">
+    <Header/>
     <form class="form-register" @submit.prevent="register">
       <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
       <div class="alert alert-danger" role="alert" v-if="registrationErrors">
@@ -39,12 +40,19 @@
         Create Account
       </button>
     </form>
+    <Footer/>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue'
 export default {
   name: 'register',
+  components: {
+    Header,
+    Footer
+  },
   data() {
     return {
       user: {
