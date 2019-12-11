c67e9c29caabc3a4a925fdca331b28694c7875c7
nathantyler <56833450+nathantyler@users.noreply.github.com>
2019-12-11 14:22:56 -0500

    Added right provided authentication classes.

--- final-capstone-springboot/src/main/java/com/techelevator/finalcapstonespringboot/model/UserRoles.java
+++ final-capstone-springboot/src/main/java/com/techelevator/finalcapstonespringboot/model/UserRoles.java
@@ -1,7 +1,10 @@
 package com.techelevator.finalcapstonespringboot.model;
 
 public class UserRoles {
-    public static final int ADMIN    = 0;
-    public static final int EMPLOYER = 1;
-    public static final int USER     = 5;
+    public static final int   ADMIN    = 0;
+    public static final int   EMPLOYER = 1;
+    public static final int   USER     = 5;
+    public static final int[] ROLES    = new int[] { ADMIN,
+                                                     EMPLOYER,
+                                                     USER };
 }
