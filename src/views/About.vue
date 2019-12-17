cf63587b97bae47e4a48fdf3680553cfd7e9c9cb
Austin Grant <entertainment360@live.com>
2019-12-17 09:38:35 -0500

    sync

---
+++ src/views/About.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="about">
+        <br>
+    <br>
+    <body>
+    <h2>About</h2>
+    <hr>
+    <p>Profolio is a social media site designed for both students and employers. 
+        Students can create an online profile and display their summary, resume, as well as projects they've completed. 
+        Employers can can browse and search through student profiles. 
+        Once a profile is created it can be updated easily on our platform. 
+        The main purpose of this site is for students to build an online profile and make it availble for employers to see.
+<br>
+This site was create as a final capstone for Tech Elevator school of software devlopment by Chris Day, Austin Grant, Nathan Newcomb and John Paul Ahme.
+<br>
+Thank you for visiting.  
+    </p>
+
+<hr>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+
+</style>
